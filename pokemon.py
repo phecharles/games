4ff5a565b0b862e67ab375f97dc629fe9b640f7e
phecharles <charlesphsouza@gmail.com>
2022-05-07 20:48:23 -0300

    Definindo a Classe Pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,22 @@
 class Pokemon:
 
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie, level=1, nome=None):
         self.tipo = tipo
         self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+
+    def __str__(self):
+        return '{}({})'.format(self.nome, self.level)
 
     def atacar(self, pokemon):
-        print('>>>> {} atacou {}'.format(self.especie, pokemon.especie))
+        print('{} atacou {}'.format(self.especie, pokemon.especie))
 
 meu_pokemon = Pokemon('Fogo', 'Charmander')
 pokemon_amigo = Pokemon('Raio', 'Pikachu')
 
-meu_pokemon.atacar(pokemon_amigo)
+print(meu_pokemon)
